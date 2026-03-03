6f6b12f43e695e268c7dbfe5e9a381e43d53ce77
Renan Cavichi <renancavichi@gmail.com>
2026-03-03 16:22:05 -0300

    add comments

--- class_notes/revisao_objetos.js
+++ class_notes/revisao_objetos.js
@@ -6,17 +6,17 @@ const user = {
     nome: "Renan",
     email: "renancavichi@gmail.com",
     idade: 41,
-    "sobre nome": "Cavichi"
+    "sobre nome": "Cavichi" // Propriedade com espaço, deve ser acessada com colchetes
 }
 
 console.log(user.nome) // Renan
-console.log(user["nome"])  // Renan
+console.log(user["nome"])  // Renan // Acessando propriedade usando colchetes, útil para propriedades com caracteres especiais ou espaços
 
-console.log(user["sobre nome"]) // Cavichi
+console.log(user["sobre nome"]) // Cavichi  // Acessando propriedade com espaço usando colchetes
+ 
+console.log(user[escolha]) // Acessando propriedade usando variável, resultado: "renancavichi@gmail.com"
 
-console.log(user[escolha])
-
-const props = {
+const props = { // Criação de um objeto props
     avatar:"https://github.com/renancavichi.png",
     name: "Renan Cavichi",
     email: "renancavichi@gmail.com"
@@ -26,7 +26,7 @@ const props = {
 //const name = props.name
 //const email = props.email
 
-const {avatar, email} = props
+const {avatar, email} = props // Desestruturação de objetos
 
 console.log(avatar) // "https://github.com/renancavichi.png"
 console.log(email) // "renancavichi@gmail.com"
