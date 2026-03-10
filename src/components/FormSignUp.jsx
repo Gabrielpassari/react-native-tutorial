987f87ddff6147b2585263cbc9d3becf48e7511f
Renan Cavichi <renancavichi@gmail.com>
2026-03-10 17:35:52 -0300

    form cadastro com handlesubmit

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,18 +1,41 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 import { TextInput } from 'react-native-web'
+import { useState } from 'react'
 
 export default function FormSignUp() {
+
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+
+  const handleSubmit = () => {
+    console.log({nome, vemail, senha})
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Cadastro</Text>
       <View style={styles.form}>
-        <TextInput style={styles.inputs} placeholder="Nome" />
-        <TextInput style={styles.inputs} placeholder="Email" />
+        <TextInput 
+            style={styles.inputs} 
+            placeholder="Nome"
+            value={nome}
+            onChangeText={setNome}
+        />
+        <TextInput 
+            style={styles.inputs} 
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+        />
         <TextInput 
             style={styles.inputs} 
             placeholder="Senha" 
             secureTextEntry
+            value={senha}
+            onChangeText={setSenha}
         />
+        <Button title="Cadastrar" onPress={handleSubmit} />
       </View>
     </View>
   )
